2fed4f230a820cfe66597d48d7c2907634d10b70
melroh629 <jw.roh629@gmail.com>
2024-12-24 08:48:42 +0900

    feat: updateElement 함수 추가

--- src/lib/updateElement.js
+++ src/lib/updateElement.js
@@ -1,8 +1,12 @@
-// import { addEvent, removeEvent } from "./eventManager";
+import { addEvent, removeEvent } from "./eventManager";
 import { createElement } from "./createElement.js";
 
 function updateAttributes(target, newProps = {}, oldProps = {}) {
-  // 이전 속성들 제거
+  // null이나 undefined인 경우 빈 객체로 처리
+  oldProps = oldProps || {};
+  newProps = newProps || {};
+
+  // 이전 속성 제거
   Object.keys(oldProps).forEach((key) => {
     if (key === "className") {
       if (!(key in newProps)) {
@@ -12,70 +16,89 @@ function updateAttributes(target, newProps = {}, oldProps = {}) {
       if (!(key in newProps)) {
         target.removeAttribute("for");
       }
-    } else if (!key.startsWith("on") && !(key in newProps)) {
-      target.removeAttribute(key);
+    } else if (key.startsWith("on")) {
+      if (!(key in newProps)) {
+        removeEvent(target, key.toLowerCase().slice(2), oldProps[key]);
+      }
+    } else {
+      if (!(key in newProps)) {
+        target.removeAttribute(key);
+      }
     }
   });
 
-  // 새로운 속성들 설정
-  Object.keys(newProps).forEach((key) => {
+  // 새로운 속성 추가/업데이트
+  Object.entries(newProps).forEach(([key, value]) => {
     if (key === "className") {
-      target.setAttribute("class", newProps[key]);
+      target.setAttribute("class", value);
     } else if (key === "htmlFor") {
-      target.setAttribute("for", newProps[key]);
-    } else if (!key.startsWith("on")) {
-      target.setAttribute(key, newProps[key]);
+      target.setAttribute("for", value);
+    } else if (key.startsWith("on")) {
+      const eventType = key.toLowerCase().slice(2);
+      if (oldProps[key] !== value) {
+        if (oldProps[key]) {
+          removeEvent(target, eventType, oldProps[key]);
+        }
+        addEvent(target, eventType, value);
+      }
+    } else {
+      target.setAttribute(key, value);
     }
   });
 }
 
-export function updateElement(parentElement, newNode, oldNode, index = 0) {
-  // 1. 이전 노드가 없는 경우 (새로운 노드 추가)
-  if (oldNode == null) {
-    parentElement.appendChild(createElement(newNode));
+export function updateElement($parent, oldNode, newNode, index = 0) {
+  // 노드가 없는 경우
+  if (!oldNode && !newNode) {
     return;
   }
-  // 2. 새로운 노드가 없는 경우 (기존 노드 제거)
-  if (newNode == null) {
-    parentElement.removeChild(parentElement.childNodes[index]);
+
+  // 새로운 노드만 있는 경우 (추가)
+  if (!oldNode) {
+    $parent.appendChild(createElement(newNode));
     return;
   }
-  // 3. 두 노드가 텍스트이고 다른 경우
+
+  // 이전 노드만 있는 경우 (제거)
+  if (!newNode) {
+    $parent.removeChild($parent.childNodes[index]);
+    return;
+  }
+
+  // 노드 타입이 다른 경우
+  if (typeof oldNode !== typeof newNode) {
+    $parent.replaceChild(createElement(newNode), $parent.childNodes[index]);
+    return;
+  }
+
+  // 텍스트 노드인 경우
   if (typeof newNode === "string" || typeof newNode === "number") {
     if (oldNode !== newNode) {
-      const oldChild = parentElement.childNodes[index];
-      const newChild = createElement(newNode);
-      parentElement.replaceChild(newChild, oldChild);
+      $parent.replaceChild(createElement(newNode), $parent.childNodes[index]);
     }
     return;
   }
-  // 4. 노드 타입이 다른 경우 (전체 교체)
+
+  // 노드 타입이 다른 경우
   if (oldNode.type !== newNode.type) {
-    const oldChild = parentElement.childNodes[index];
-    const newChild = createElement(newNode);
-    parentElement.replaceChild(newChild, oldChild);
+    $parent.replaceChild(createElement(newNode), $parent.childNodes[index]);
     return;
   }
 
-  // 5. 같은 타입의 엘리먼트인 경우
-  // 5.1. 속성 업데이트
-  updateAttributes(
-    parentElement.childNodes[index],
-    newNode.props,
-    oldNode.props,
-  );
+  // 속성 업데이트
+  updateAttributes($parent.childNodes[index], newNode.props, oldNode.props);
 
-  // 5.2. 자식 노드들 재귀적으로 업데이트
+  // 자식 노드 업데이트
   const maxLength = Math.max(
-    newNode.children?.length || 0,
     oldNode.children?.length || 0,
+    newNode.children?.length || 0,
   );
 
   for (let i = 0; i < maxLength; i++) {
     updateElement(
-      parentElement.childNodes[index],
-      newNode.children?.[i],
+      $parent.childNodes[index],
       oldNode.children?.[i],
+      newNode.children?.[i],
       i,
     );
   }
