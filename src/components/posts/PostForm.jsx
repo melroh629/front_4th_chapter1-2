e3f68cc2045c400d6860916167891d55381aef30
melroh629 <jw.roh629@gmail.com>
2024-12-26 10:43:28 +0900

    feat: Post작성 기능 추가(로그인 사용자에 한해)

--- src/components/posts/PostForm.jsx
+++ src/components/posts/PostForm.jsx
@@ -1,7 +1,25 @@
 /** @jsx createVNode */
 import { createVNode } from "../../lib";
+import { globalStore } from "../../stores";
 
 export const PostForm = () => {
+  const { currentUser } = globalStore.getState();
+  const handleAddPost = () => {
+    const content = document.getElementById("post-content").value;
+    globalStore.setState({
+      posts: [
+        {
+          id: Date.now(),
+          author: currentUser.username,
+          time: Date.now(),
+          content,
+          likeUsers: [],
+        },
+        ...globalStore.getState().posts,
+      ],
+    });
+    document.getElementById("post-content").value = "";
+  };
   return (
     <div className="mb-4 bg-white rounded-lg shadow p-4">
       <textarea
@@ -11,6 +29,7 @@ export const PostForm = () => {
       />
       <button
         id="post-submit"
+        onClick={handleAddPost}
         className="mt-2 bg-blue-600 text-white px-4 py-2 rounded"
       >
         게시
